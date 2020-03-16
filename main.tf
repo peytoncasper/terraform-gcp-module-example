3b6391e22371f88ca7f18f2d6840ceb719c8bfc9
Peyton Casper <peytoncas@gmail.com>
2020-03-16 12:28:47 -0400

    Updated zone

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "web" {
   name         = "web"
   machine_type = var.instance_type
-  zone         = "us-east1-a"
+  zone         = "us-east1-b"
   count        = 1
 
   boot_disk {
